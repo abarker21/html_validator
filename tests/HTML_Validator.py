886826f2f36a8a94c7bedf1a824e2f4fdf48a3f1
Alex Barker <abarker21@cmc.edu>
2020-02-14 21:59:20 -0800

    1

--- tests/HTML_Validator.py
+++ tests/HTML_Validator.py
@@ -1,90 +1,62 @@
-#!/bin/python3
-
-
-def validate_html(html):
-    '''
-    This function performs a limited version of html validation by checking whether every opening t$
+ction performs a limited version of html validation by checking whether every opening tag $
     >>> validate_html('<strong>example</strong>')
     True
     >>> validate_html('<strong>example')
     False
     '''
 
-    list1 = _extract_tags(html);
-
+    phrase = _extract_tags(html);
     i = 0
-
-    while i < len(list1)-1:
-
-       
-        
-        tag0 = list1[i]
-        tag1 = "<" + "/" + tag0[1:]
-
-        if list1[i+1] != tag1:
-            tag0 = list1[i+1]
-            tag1 = "<" + "/" + tag0[1:]
+    while i < len(phrase)-1:
+        tag = phrase[i]
+        nexttag = "<" + "/" + tag[1:]
+        if phrase[i+1] != nexttag:
+            tag = phrase[i+1]
+            nexttag = "<" + "/" + tag[1:]
             i+=1
-            if tag0[1:2] == '/':
+            if tag[1:2] == '/':
                 return False
-            
         else:
-            list1.remove(tag0)
-            list1.remove(tag1)
+            phrase.remove(tag)
+            phrase.remove(nexttag)
             i = 0
-
-        print(list1)
-        
-
-
-    print(list1)
-    if (len(list1) != 0):
+        print(phrase)
+    print(phrase)
+    if (len(phrase) != 0):
         return False
     else:
         return True
 
-    
 
-    
+    # HINT:
+    # use the _extract_tags function below to generate a list of html tags without any extra text;
+    # then process these html tags using the balanced parentheses algorithm from the book
+    # the main difference between your code and the book's code will be that you will have to keep tra$
+    # but arbitrary text located between the html tags
+
 
 
 def _extract_tags(html):
     '''
     This is a helper function for `validate_html`.
-    By convention in Python, helper functions that are not meant to be used directly by the user ar$
+    By convention in Python, helper functions that are not meant to be used directly by the user are p$
     This function returns a list of all the html tags contained in the input string,
     stripping out all text not contained within angle brackets.
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
     '''
 
-    list1 = list(html)
-    list_out = []
-
+    phrase = list(html)
+    output = []
     i = 0
-
-    while i < len(list1):
-
-        thing = list1[i]
-        
-        if thing == '<':
-            
-           
-            
-            word1 = ""
-            
-            while list1[i] != '>':
-                word1 = word1 + list1[i]
-                
+    while i < len(phrase):
+        character = phrase[i]
+        if character == '<':
+            word = ""
+            while phrase[i] != '>':
+                word = word + phrase[i]
                 i = i+1
-            word1 = word1 + list1[i]
-            
-            list_out.append(word1)
-            
-            
+            word = word + phrase[i]
+            output.append(word)
         i = i + 1
-
-    return list_out
-
-    
-
+    return output
