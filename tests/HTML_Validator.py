f19c3462f11b7c2020fc9273ea2d8c51cc3cc8d3
Alex Barker <abarker21@cmc.edu>
2020-02-14 22:20:00 -0800

    1

--- tests/HTML_Validator.py
+++ tests/HTML_Validator.py
@@ -10,36 +10,36 @@ def validate_html(html):
     False
     '''
 
-    phrase = _extract_tags(html);
+    list_storage = _extract_tags(html);
+
     i = 0
-    while i < len(phrase)-1:
-        tag = phrase[i]
-        next = "<" + "/" + tag[1:]
-        if phrase[i+1] != next:
-            tag = phrase[i+1]
-            next = "<" + "/" + tag[1:]
+
+    while i < len(list_storage)-1:   
+        tag0 = list_storage[i]
+        tag1 = "<" + "/" + tag0[1:]
+
+        if list_storage[i+1] != tag1:
+            tag0 = list_storage[i+1]
+            tag1 = "<" + "/" + tag0[1:]
             i+=1
-            if tag[1:2] == '/':
+            if tag0[1:2] == '/':
                 return False
+            
         else:
-            phrase.remove(tag)
-            phrase.remove(next)
+            list_storage.remove(tag0)
+            list_storage.remove(tag1)
             i = 0
-        print(phrase)
-    print(phrase)
-    if (len(phrase) != 0):
+
+        print(list_storage)
+        
+
+
+    print(list_storage)
+    if (len(list_storage) != 0):
         return False
     else:
         return True
-
-
-    # HINT:
-    # use the _extract_tags function below to generate a list of html tags without any extra text;
-    # then process these html tags using the balanced parentheses algorithm from the book
-    # the main difference between your code and the book's code will be that you will have to keep tra$
-    # but arbitrary text located between the html tags
-
-
+        
 def _extract_tags(html):
     '''
     This is a helper function for `validate_html`.
@@ -50,11 +50,25 @@ def _extract_tags(html):
     ['<strong>', '</strong>']
     '''
 
-    phrase = list(html)
-    output = []
+    list_storage = list(html)
+    p_out = []
     i = 0
-    while i < len(phrase):
-        character = phrase[i]
-        if character == '<':
-                                                                               1,14          Top
+    while i < len(list_storage):
 
+        char7 = list_storage[i]
+        
+        if char7 == '<':
+  
+            word1 = ""     
+            while list_storage[i] != '>':
+                word1 = word1 + list_storage[i]
+                
+                i = i+1
+            word1 = word1 + list_storage[i]
+            
+            p_out.append(word1)
+            
+            
+        i = i + 1
+
+    return p_out
